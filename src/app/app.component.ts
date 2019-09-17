891a81e32d62b03e2f00c637276ab09415cacc34
alifdelgado <alifdelgado@gmail.com>
2019-09-17 11:04:13 -0500

    Se termina la aplicacion de miscelaneos, no se agregan las ventanas emergentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,37 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AjustesProvider } from '../providers/ajustes/ajustes';
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  // rootPage:any = HomePage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private ajustes: AjustesProvider) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.ajustes.cargar_storage().then(() => {
+        if(this.ajustes.ajustes.mostrar_tutorial) {
+          this.rootPage = "IntroduccionPage";
+        }else{
+          this.rootPage = HomePage;
+        }
+
+        this.platform.pause.subscribe( () => {
+          console.log("la aplicación se detendra");
+        });
+
+        this.platform.resume.subscribe( () => {
+          console.log("la aplicación va a continuar");
+        });
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+      });
     });
   }
 }
---
+++ src/providers/ajustes/ajustes.ts
@@ -0,0 +1,54 @@
+import { Storage } from '@ionic/storage';
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+/*
+  Generated class for the AjustesProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class AjustesProvider {
+
+  ajustes = {
+    mostrar_tutorial: true
+  }
+
+  constructor(private platform: Platform, private storage: Storage) {
+    console.log('Hello AjustesProvider Provider');
+  }
+
+  cargar_storage() {
+    let promesa = new Promise((resolve, reject) => {
+      if(this.platform.is("cordova")) {
+        console.log("inicializando storage");
+        this.storage.ready().then(() => {
+          this.storage.get("ajustes").then(ajustes => {
+            if(ajustes) {
+              this.ajustes = ajustes;
+            }
+            resolve();
+          });
+        });
+      }else{
+        if(localStorage.getItem("ajustes")) {
+          this.ajustes = JSON.parse(localStorage.getItem("ajustes"));
+        }
+        resolve();
+      }
+    });
+    return promesa;
+  }
+
+  guardar_storage() {
+    if(this.platform.is("cordova")) {
+      this.storage.ready().then(() => {
+        this.storage.set("ajustes", this.ajustes);
+      });
+    }else{
+      localStorage.setItem("ajustes", JSON.stringify(this.ajustes));
+    }
+  }
+
+}
